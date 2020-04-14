a6bd8cb3a86d44070dda4bea9ba1734b5f0229a6
Edward Tanoto <ed27tanoto@gmail.com>
2020-04-15 00:22:26 +0700

    commit

---
+++ lib/main.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import './screens/splash_screen.dart';
+import './screens/cart_screen.dart';
+import './screens/products_overview_screen.dart';
+import './screens/product_detail_screen.dart';
+import './providers/products.dart';
+import './providers/cart.dart';
+import './providers/orders.dart';
+import './providers/auth.dart';
+import './screens/orders_screen.dart';
+import './screens/user_products_screen.dart';
+import './screens/edit_product_screen.dart';
+import './screens/auth_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider.value(value: Auth()),
+        ChangeNotifierProxyProvider<Auth, Products>(
+          builder: (context, auth, previousProduct) => Products(
+              auth.token,
+              auth.userId,
+              previousProduct == null ? [] : previousProduct.items),
+        ),
+        ChangeNotifierProvider.value(
+          //builder: (ctx) => Cart(),
+          value: Cart(),
+        ),
+        ChangeNotifierProxyProvider<Auth, Orders>(
+          builder: (ctx, auth, previousOrder) => Orders(auth.token, auth.userId,
+              previousOrder == null ? [] : previousOrder.orders),
+        )
+      ],
+      child: Consumer<Auth>( 
+        builder: (context, auth, _) => MaterialApp(
+          title: 'MyShop',
+          theme: ThemeData(
+            primarySwatch: Colors.lightBlue,
+            accentColor: Colors.white60,
+            fontFamily: 'Lato',
+          ),
+          home: auth.isAuth
+              ? ProductsOverviewScreen()
+              : FutureBuilder(
+                  future: auth.tryAutoLogin(),
+                  builder: (ctx, authResultSnapshot) => 
+                  authResultSnapshot.connectionState == ConnectionState.waiting? SplashScreen() : AuthScreen()),
+          routes: {
+            ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
+            CartScreen.routeName: (ctx) => CartScreen(),
+            OrdersScreen.routeName: (ctx) => OrdersScreen(),
+            UserProductsScreen.routeName: (ctx) => UserProductsScreen(),
+            EditProductScreen.routeName: (ctx) => EditProductScreen(),
+          },
+        ),
+      ),
+    );
+  }
+}
